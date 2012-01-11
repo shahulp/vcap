3f6546e24ffde4c9eb7caa7a891c0cad399ebbbc
Shahul <shahul.p@gmail.com>
2012-01-10 21:54:24 -0800

    Auto staging

--- staging/lib/vcap/staging/plugin/virgo/virgo.rb
+++ staging/lib/vcap/staging/plugin/virgo/virgo.rb
@@ -2,6 +2,13 @@ require 'fileutils'
 require 'yaml'
 
 class Virgo
+  AUTOSTAGING_JAR = 'com.sap.icp.autostager-1.0.0.PTYP.jar'
+  AUTOSTAGING_DEP =['com.springsource.org.codehaus.jackson-1.4.3.jar','com.springsource.org.codehaus.jackson.mapper-1.4.3.jar',
+                     'com.springsource.org.joda.time-1.5.2.jar' ]
+  SERVICE_DRIVER_HASH = {
+      "mysql-5.1" => 'com.springsource.com.mysql.jdbc-5.1.6.jar',
+      "postgresql-9.0" => 'com.springsource.org.postgresql.jdbc4-8.3.604.jar'
+  }
 
   def self.resource_dir
     File.join(File.dirname(__FILE__), 'resources')
@@ -23,11 +30,37 @@ class Virgo
     FileUtils.mkdir_p(webapp_path)
     webapp_path
   end
-
+  
+  def self.copy_jar(jar, dest)    
+    jar_path = File.join(File.dirname(__FILE__), 'resources', jar)
+    FileUtils.mkdir_p dest
+    FileUtils.cp(jar_path, dest)
+  end
+  
+  def self.copy_service_drivers(services, webapp_root)
+    drivers = services.select { |svc|
+      SERVICE_DRIVER_HASH.has_key?(svc[:label])
+    }
+    drivers.each { |driver|      
+      driver_dest = File.join(webapp_root, "..", Virgo.repository,"usr")
+      copy_jar SERVICE_DRIVER_HASH[driver[:label]], driver_dest
+    } if drivers
+  end
+  
+  def self.prepare_stager(webapp_root)
+    dest = File.join(webapp_root, "..", Virgo.repository,"usr")
+     copy_jar AUTOSTAGING_JAR , dest
+     AUTOSTAGING_DEP.each{ |jar|
+        copy_jar jar , dest
+       }
+  end
+  
   def self.detect_file_extension(webapppath)
     Dir.chdir(webapppath) do
       if !Dir.glob("*.plan").empty?
         return "plan"
+      elsif !Dir.glob("*.par").empty?
+        return "parpacked"
       end
     end
 
